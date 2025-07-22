73e170f54fa352e59ada5058bf05a49b4daff01c
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-22 16:11:00 -0300

    Add: construction gif to incomplete pages

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1>About Me</h1>
-      <p>Welcome to my blog! I'm Bruno R Garcia, a passionate software developer and tech enthusiast.</p>
-      <p>Here, I share my thoughts, experiences, and projects related to programming, technology, and more.</p>
-      <p>Feel free to explore my posts and connect with me!</p>
-    </div>
+    <Image
+      style={{ display: "block", margin: "0 auto" }}
+      src="/images/construction.gif"
+      alt="Under Construction"
+      width={600}
+      height={200}
+    />
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,13 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        
+        <Image
+              style={{ display: "block", margin: "0 auto" }}
+              src="/images/construction.gif"
+              alt="Under Construction"
+              width={600}
+              height={200}
+            />
       </main>
     </div>
   );
