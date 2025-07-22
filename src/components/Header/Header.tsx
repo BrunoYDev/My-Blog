9cc6864f4c1d9f2ce0834962279e790d1ea04df6
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-22 15:15:01 -0300

    Create: Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from './Header.module.css';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link href="/" className={styles.navLink}>✧ Home ✧</Link>
+        <Link href="/blog" className={styles.navLink}>✧ Blog ✧</Link>
+        <Link href="/about" className={styles.navLink}>✧ Sobre ✧</Link>
+      </nav>
+    </header>
+  );
+}
