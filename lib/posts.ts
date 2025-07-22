26c9c3574cc5de655da58e711b02b8c468aba76e
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-22 15:15:19 -0300

    Create: posts scrapper

---
+++ lib/posts.ts
@@ -0,0 +1,56 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export function getSortedPostsData() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.mdx$/, '');
+
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+    const matterResult = matter(fileContents);
+
+    return {
+      id,
+      ...(matterResult.data as { title: string; date: string; author: string; excerpt: string }),
+    };
+  });
+
+  return allPostsData.sort((a, b) => {
+    if (a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+
+  }); 
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        slug: fileName.replace(/\.mdx$/, ''),
+      },
+    };
+  });
+}
+
+export async function getPostData(slug: string) {
+  const fullPath = path.join(postsDirectory, `${slug}.mdx`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  const matterResult = matter(fileContents);
+
+  return {
+    slug,
+    content: matterResult.content,
+    ...(matterResult.data as { title: string; date: string; author: string }),
+  };
+}
