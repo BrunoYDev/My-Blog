a6ceacbb0a1061fffc310969f9d7d9b9ab0815f8
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-28 17:45:49 -0300

    Fix: ESLint Linting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,7 +10,7 @@ export default function NotFound() {
           <span>ERROR</span>
         </p>
         <p>A fatal exception 404 has occurred at 0028:C0011E36 in VXD VMM(01) + 00010E36.</p>
-        <p>The requested page could not be found in the server's memory banks. It may have been moved, deleted, or it might be hiding in another castle.</p>
+        <p>The requested page could not be found in the server&apos;s memory banks. It may have been moved, deleted, or it might be hiding in another castle.</p>
         <br />
         <p>* Press any key to terminate the current application.</p>
         <p>* Press CTRL+ALT+DEL to restart your browser. You will lose any unsaved data in all applications.</p>
