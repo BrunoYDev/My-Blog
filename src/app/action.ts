79fb86e5f3ceacfbdd9ee952d209a774ee7c3d72
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-28 17:17:49 -0300

    Add: Supabase Actions

--- src/app/action.ts
+++ src/app/action.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { supabase } from '../../lib/supabaseClient';
+import { revalidatePath } from 'next/cache';
 import { kv } from '@vercel/kv';
 
 const getTodayKey = () => {
@@ -17,4 +19,24 @@ export async function registerVisit(isFirstVisitEver: boolean, isNewDay: boolean
     const todayKey = getTodayKey();
     await kv.incr(todayKey);
   }
+}
+
+export async function addGuestbookEntry(formData: FormData) {
+  const username = formData.get('username') as string;
+  const message = formData.get('message') as string;
+
+  if (!username.trim() || !message.trim()) {
+    return { error: 'Error: Name and message cannot be empty.' };
+  }
+
+  const { error } = await supabase
+    .from('guestbook_entries')
+    .insert([{ username, message }]);
+  
+  if (error) {
+    console.error('Supabase error:', error);
+    return { error: 'Error: Could not save the message.' };
+  }
+  revalidatePath('/guestbook');
+  return { success: true };
 }
