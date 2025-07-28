d3c6a334b2d80c3393c5aca6befa336193b36e0f
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-28 09:37:11 -0300

    Change: Action.ts to multiple counters

--- src/app/action.ts
+++ src/app/action.ts
@@ -2,6 +2,19 @@
 
 import { kv } from '@vercel/kv';
 
-export async function incrementVisitorCount() {
-  await kv.incr('pageviews');
+const getTodayKey = () => {
+  const today = new Date().toISOString().split('T')[0];
+  return `daily_visits_${today}`;
+};
+
+
+export async function registerVisit(isFirstVisitEver: boolean, isNewDay: boolean) {
+  if (isFirstVisitEver) {
+    await kv.incr('total_unique_visitors');
+  }
+
+  if (isNewDay) {
+    const todayKey = getTodayKey();
+    await kv.incr(todayKey);
+  }
 }
