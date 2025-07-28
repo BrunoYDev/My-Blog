cc4b2f794f03251a919474761eedb0030cc4da6a
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-28 10:36:32 -0300

    Change: google analytics to manual script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header/Header";
-import { GoogleTagManager } from '@next/third-parties/google';
+import { GoogleTagManager } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "BrunoRGarcia Blog",
-  description: "Welcome to my blog, my personal space on the internet where I share my thoughts, experiences, and projects.",
+  description:
+    "Welcome to my blog, my personal space on the internet where I share my thoughts, experiences, and projects.",
   viewport: {
-    width: 'device-width',
+    width: "device-width",
     initialScale: 1,
   },
   icons: {
-    icon: '/favicon.gif',
+    icon: "/favicon.gif",
   },
 };
 
@@ -22,10 +23,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}
+        ></script>
+        <script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || []; function gtag()
+          {dataLayer.push(arguments)}
+          gtag('js', new Date()); gtag('config', '${process.env.GTM_ID}');
+          `}
+        </script>
+      </head>
       <body className={``}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM_ID}`}
+            height={"0"}
+            width={"0"}
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
-        <GoogleTagManager gtmId={process.env.GTM_ID!} />
       </body>
     </html>
   );
