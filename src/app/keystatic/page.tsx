7b04828be9a12fd0a8a698dafedb095e0544175e
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-30 16:00:44 -0300

    FIX: Typescript any error

--- src/app/keystatic/page.tsx
+++ src/app/keystatic/page.tsx
@@ -2,7 +2,8 @@
 
 import { Keystatic } from '@keystatic/core/ui';
 import config from '../../../keystatic.config';
+import { Config } from '@keystatic/core';
 
 export default function KeystaticPage() {
-  return <Keystatic config={config as any} />;
+  return <Keystatic config={config as Config} />;
 }
