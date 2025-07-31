363fd37c2c159e5ae0fb0f78afc803c0fb419be9
Bruno Rodrigues Garcia <118909147+BrunoYDev@users.noreply.github.com>
2025-07-31 15:19:45 -0300

    Fix: ViewCounter CI counting

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,6 @@ import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-
-  env: {
-    CI: process.env.CI,
-  },
-
 };
 
 const withMDX = createMDX({
--- src/components/ViewCounterTrigger/ViewCounterTrigger.tsx
+++ src/components/ViewCounterTrigger/ViewCounterTrigger.tsx
@@ -1,37 +1,35 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
-import { registerVisit } from '../../app/action';
+import { useEffect } from "react";
+import { registerVisit } from "../../app/action";
 
 export function ViewCounterTrigger() {
-  
   useEffect(() => {
+    if (process.env.NODE_ENV == "production") {
+      const TOTAL_VISIT_KEY = "has_visited_ever";
+      const DAILY_VISIT_KEY = "last_visit_timestamp";
+      const COOLDOWN_PERIOD = 24 * 60 * 60 * 1000;
 
-    if (process.env.NODE_ENV != 'production' || process.env.CI) {
-      console.log('Development or build environment detected, skipping view count.');
-      return;
-    }
+      const now = Date.now();
 
-    const TOTAL_VISIT_KEY = 'has_visited_ever';
-    const DAILY_VISIT_KEY = 'last_visit_timestamp';
-    const COOLDOWN_PERIOD = 24 * 60 * 60 * 1000; // 24 horas em milissegundos
+      const hasVisitedEver = localStorage.getItem(TOTAL_VISIT_KEY) === "true";
 
-    const now = Date.now();
+      const lastVisitTimestamp = parseInt(
+        localStorage.getItem(DAILY_VISIT_KEY) || "0"
+      );
 
-    const hasVisitedEver = localStorage.getItem(TOTAL_VISIT_KEY) === 'true';
+      const isNewDay =
+        !lastVisitTimestamp || now - lastVisitTimestamp > COOLDOWN_PERIOD;
 
-    const lastVisitTimestamp = parseInt(localStorage.getItem(DAILY_VISIT_KEY) || '0');
-    
-    const isNewDay = !lastVisitTimestamp || (now - lastVisitTimestamp > COOLDOWN_PERIOD);
-    
-    if (!hasVisitedEver || isNewDay) {
-      registerVisit(!hasVisitedEver, isNewDay);
-      
-      if (!hasVisitedEver) {
-        localStorage.setItem(TOTAL_VISIT_KEY, 'true');
+      if (!hasVisitedEver || isNewDay) {
+        registerVisit(!hasVisitedEver, isNewDay);
+
+        if (!hasVisitedEver) {
+          localStorage.setItem(TOTAL_VISIT_KEY, "true");
+        }
+
+        localStorage.setItem(DAILY_VISIT_KEY, now.toString());
       }
-
-      localStorage.setItem(DAILY_VISIT_KEY, now.toString());
     }
   }, []);
 
